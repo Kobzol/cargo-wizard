dce79e0569eb6e853ba41b754da89619f2ba0d42
Jakub Beránek <jakub.beranek@vsb.cz>
2024-03-07 16:39:27 +0100

    Add a test for declining a diff

--- tests/integration/dialog.rs
+++ tests/integration/dialog.rs
@@ -1,7 +1,5 @@
 use crate::utils::{init_cargo_project, CargoProject};
 
-// TODO: decline diff
-
 #[test]
 fn dialog_fast_compile_to_dev() -> anyhow::Result<()> {
     let project = init_cargo_project()?;
@@ -36,6 +34,28 @@ fn dialog_fast_compile_to_release() -> anyhow::Result<()> {
 
     [profile.release]
     debug = 0
+    "###);
+
+    assert!(!project.file_exists(project.config_path()));
+
+    Ok(())
+}
+
+#[test]
+fn dialog_deny_diff() -> anyhow::Result<()> {
+    let project = init_cargo_project()?;
+
+    DialogBuilder::default()
+        .release_template()
+        .accept_diff(false)
+        .run(&project)?;
+
+    insta::assert_snapshot!(project.read_manifest(), @r###"
+
+    [package]
+    name = "foo"
+    version = "0.1.0"
+    edition = "2021"
     "###);
 
     assert!(!project.file_exists(project.config_path()));
@@ -187,31 +207,80 @@ rustflags = ["-Ctarget-cpu=native"]
     Ok(())
 }
 
+struct DialogBuilder {
+    profile_index: u32,
+    template_index: u32,
+    accept_diff: bool,
+}
+
+impl Default for DialogBuilder {
+    fn default() -> Self {
+        Self {
+            profile_index: 0,
+            template_index: 0,
+            accept_diff: true,
+        }
+    }
+}
+
+impl DialogBuilder {
+    fn profile(mut self, index: u32) -> Self {
+        self.profile_index = index;
+        self
+    }
+
+    fn template(mut self, index: u32) -> Self {
+        self.template_index = index;
+        self
+    }
+
+    fn release_template(self) -> Self {
+        self.template(1)
+    }
+
+    fn accept_diff(mut self, value: bool) -> Self {
+        self.accept_diff = value;
+        self
+    }
+
+    fn run(self, project: &CargoProject) -> anyhow::Result<()> {
+        let mut terminal = project.cmd(&[]).start_terminal()?;
+        // Select template
+        for _ in 0..self.template_index {
+            terminal.key_down()?;
+        }
+        terminal.key_enter()?;
+        // Select profile
+        for _ in 0..self.profile_index {
+            terminal.key_down()?;
+        }
+        terminal.key_enter()?;
+        // Customize template
+        terminal.key_enter()?;
+        // Handle diff
+        if self.accept_diff {
+            terminal.line("y")?;
+        } else {
+            terminal.line("n")?;
+        }
+        terminal.wait()
+    }
+}
+
 fn apply_fast_runtime_to_release(project: &CargoProject) -> anyhow::Result<()> {
-    apply_profile(project, 1, 1)
+    DialogBuilder::default()
+        .release_template()
+        .profile(1)
+        .run(project)
 }
 
 fn apply_profile(
     project: &CargoProject,
-    template_index: u64,
-    profile_index: u64,
+    template_index: u32,
+    profile_index: u32,
 ) -> anyhow::Result<()> {
-    let mut terminal = project.cmd(&[]).start_terminal()?;
-    // Select template
-    for _ in 0..template_index {
-        terminal.key_down()?;
-    }
-    terminal.key_enter()?;
-    // Select profile
-    for _ in 0..profile_index {
-        terminal.key_down()?;
-    }
-    terminal.key_enter()?;
-    // Customize template
-    terminal.key_enter()?;
-    // Confirm diff
-    terminal.line("y")?;
-    terminal.wait()?;
-
-    Ok(())
+    DialogBuilder::default()
+        .template(template_index)
+        .profile(profile_index)
+        .run(project)
 }
